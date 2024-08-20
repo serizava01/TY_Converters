9b42b4a145efc37e86e88055a8f1b7c7b5af3cea
serizava01 <ausiferausifer@gmail.com>
2024-08-20 21:30:04 +0700

    Add project files.

---
+++ TY_Converters/Main.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TY_Converters
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bw_01 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(391, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Converter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(84, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ข้อความระบบ :";
+            // 
+            // bw_01
+            // 
+            this.bw_01.WorkerReportsProgress = true;
+            this.bw_01.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_01_DoWork);
+            this.bw_01.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bw_01_ProgressChanged);
+            this.bw_01.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_01_RunWorkerCompleted);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "URL";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(632, 201);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ควบคุม";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 184);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(620, 11);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(289, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(96, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(289, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(96, 119);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(289, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(656, 226);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainFrom";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.ComponentModel.BackgroundWorker bw_01;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ TY_Converters/Main.cs
@@ -0,0 +1,145 @@
+﻿using NAudio.Lame;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace TY_Converters
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ProcessVideo(string videoUrl)
+        {
+            try
+            {
+                var youtube = new YoutubeClient();
+                var video = youtube.Videos.GetAsync(videoUrl).Result;
+                string videoTitle = video.Title;
+                string outputPath = null;
+
+                // เรียกใช้งาน SaveFileDialog บน UI thread
+                this.Invoke(new Action(() =>
+                {
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3";
+                        saveFileDialog.FileName = videoTitle + ".mp3";
+
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            outputPath = saveFileDialog.FileName;
+                        }
+                    }
+                }));
+
+                if (!string.IsNullOrEmpty(outputPath))
+                {
+                    var streamManifest = youtube.Videos.Streams.GetManifestAsync(video.Id).Result;
+                    var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+                    if (streamInfo != null)
+                    {
+                        var stream = youtube.Videos.Streams.GetAsync(streamInfo).Result;
+                        string tempFilePath = Path.GetTempFileName();
+
+                        using (var fileStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write))
+                        {
+                            stream.CopyTo(fileStream);
+                        }
+
+                        using (var mp4 = new MediaFoundationReader(tempFilePath))
+                        using (var mp3 = new LameMP3FileWriter(outputPath, mp4.WaveFormat, 128))
+                        {
+                            mp4.CopyTo(mp3);
+                        }
+
+                        if (File.Exists(tempFilePath))
+                        {
+                            File.Delete(tempFilePath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error processing video: {videoUrl}\n{ex.Message}", "ข้อผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var urls = new List<string>
+            { textBox1.Text,
+            textBox2.Text,
+            textBox3.Text,
+            textBox4.Text,
+            textBox5.Text
+            }.Where(url => !String.IsNullOrEmpty(url)).ToList();
+            if (!urls.Any())
+            {
+                MessageBox.Show("กรุณาใส่ Url ก่อนทำการแฟปลงไฟล์", "ข้อผิดพลาด",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            videoUrls = urls;
+            currentUrlIndex = 0;
+            label1.Text = "กำลังทำงาน...";
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 100;
+            if(!bw_01.IsBusy)
+            {
+                bw_01.RunWorkerAsync();
+            }
+
+
+        }
+
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private List<string> videoUrls;
+        private int currentUrlIndex;
+        private async void bw_01_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (currentUrlIndex < videoUrls.Count)
+            {
+                string videoUrl = videoUrls[currentUrlIndex];
+                ProcessVideo(videoUrl);
+                currentUrlIndex++;
+                
+                bw_01.ReportProgress((currentUrlIndex * 100) / videoUrls.Count);
+            }
+
+        }
+
+        private void bw_01_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            label1.Text = "การทำงานเสร็จสิ้น!";
+            progressBar1.Style = ProgressBarStyle.Blocks;
+        }
+
+        private void bw_01_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+    }
+}
